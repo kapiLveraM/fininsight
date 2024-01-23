91b9a22a01b12bbaa7dd7a0458fcd9f2f4f67fa9
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-23 13:58:53 +0530

    start our project

--- src/components/common/route/Dashboard.tsx
+++ src/components/common/route/Dashboard.tsx
@@ -1,13 +1,13 @@
-import  {Route, Routes, } from "react-router-dom"
-import Details from "../../../view/dashboards/Details"
+import { Route, Routes } from "react-router-dom";
+import Details from "../../../view/dashboards/Details";
 
 const Dashboard = () => {
   return (
-      <Routes>
-          <Route path="/" element={<Details/>}/>
-          <Route path="/" element={<Details/>}/>
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/details" element={<Details />} />
+    </Routes>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
